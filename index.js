4f0d3c6e49c30c271764f8e970ef04e9d0fcbe01
vincenttommi <vincenttommi@gmail.com>
2023-03-13 19:15:50 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+
+function moveDodgerLeft() {
+    const left = parseInt(dodger.style.left);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 4}px`;
+    }
+  }
+  
+// document.addEventListener("Keydown", function (e){
+//     if (e.key === "ArrowLeft"){
+
+//         moveDodgerLeft();
+
+//     }
+// });
+
+
+
+function moveDodgerRight() {
+    var leftNumbers = DODGER.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10);
+  
+    if (left < GAME_WIDTH - DODGER_WIDTH) {
+      DODGER.style.left = `${left + DODGER_SPEED}px`;
+    }
+  }
+  
